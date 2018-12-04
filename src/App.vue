dfbc5ff8c08a91677e3427d4edbc4d5e9ebe9ea6
ygxdxx <aaa100007673@163.com>
2018-12-04 15:50:21 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello worl
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App'
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~common/stylus/variable.styl"
+
+  #app
+    color $color-theme
+</style>
