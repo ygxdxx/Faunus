1073523c41fabcf06d09730695383118fa5b7435
ygxdxx <aaa100007673@163.com>
2018-12-11 22:02:09 +0800

    litte

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -46,6 +46,5 @@ export function prefixStyle (style) {
   if (vendor === 'standard') {
     return style
   }
-  console.log(vendor + style.charAt(0).toUpperCase() + style.substr(1))
   return vendor + style.charAt(0).toUpperCase() + style.substr(1)
 }
